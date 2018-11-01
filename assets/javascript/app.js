196f69b5fd453d377109c7a6fc13048e8f4f376d
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-10-31 20:04:05 -0500

    started working on logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+//GOOGLE BOOKS API
+// var book = "Harry Potter";
+// var searchGoogleBooks = function(book) {
+//     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + book;
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//     });
+// };
