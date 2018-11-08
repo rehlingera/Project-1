3482e0df5c11b4e68850abda0b8689562a2560f2
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-11-07 19:53:46 -0600

    made it so a random choice is displayed in each column when a search is entered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
 var callEdamam = function(searchTerm) {
-    var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=9337f61e&app_key=cfaff60bede4f57a26d84e860a2b3048&from=0&to=1";
+    var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=9337f61e&app_key=cfaff60bede4f57a26d84e860a2b3048&from=0&to=20";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
         $("#aDiv3").empty();
+        n = Math.floor((Math.random() * response.hits.length))
         var recipeDiv = $("<div>");
-        recipeDiv.append("<h4>" + response.hits[0].recipe.label + "</h4>");
-        recipeDiv.append("<img style='width:100%' src='" + response.hits[0].recipe.image + "'><ul>");
+        recipeDiv.append("<h4>" + response.hits[n].recipe.label + "</h4>");
+        recipeDiv.append("<img style='width:100%' src='" + response.hits[n].recipe.image + "'><ul>");
         for (i=0;i<response.hits[0].recipe.ingredientLines.length;i++) {
             recipeDiv.append("<li>" + response.hits[0].recipe.ingredientLines[i] + "</li>")
         };
@@ -19,40 +20,52 @@ var callEdamam = function(searchTerm) {
 };
 
 var callGoogleBooks = function(searchTerm) {
-    var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&key=AIzaSyAr0_LkySyKDBgp1lJhABUZ8tUaoD5wyac";
+    var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&maxResults=20&key=AIzaSyAr0_LkySyKDBgp1lJhABUZ8tUaoD5wyac";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
+        n = Math.floor((Math.random() * response.items.length))
         $("#aDiv1").empty();
         var bookDiv = $("<div>");
-        bookDiv.append("<h4>" + response.items[0].volumeInfo.title + "</h4>");
-        bookDiv.append("<img style='width:100%' src='" + response.items[0].volumeInfo.imageLinks.thumbnail + "'>")
-        bookDiv.append("<h5>" + response.items[0].volumeInfo.authors[0] + "</h5>");
-        bookDiv.append("<p>" + response.items[0].volumeInfo.description + "</p>");
-        bookDiv.append("<a href='" + response.items[0].volumeInfo.previewLink + "'>Go to preview</a>");
+        bookDiv.append("<h4>" + response.items[n].volumeInfo.title + "</h4>");
+        bookDiv.append("<img style='width:100%' src='" + response.items[n].volumeInfo.imageLinks.thumbnail + "'>")
+        bookDiv.append("<h5>" + response.items[n].volumeInfo.authors[0] + "</h5>");
+        bookDiv.append("<p>" + response.items[n].volumeInfo.description + "</p>");
+        bookDiv.append("<a href='" + response.items[n].volumeInfo.previewLink + "'>Go to preview</a>");
         $("#aDiv1").append(bookDiv);
     });
 };
 
 var callOMDB = function(searchTerm) {
-    var queryURL = "http://www.omdbapi.com/?apikey=9addc862&t=" + searchTerm + "&plot=full";
+    var queryURL = "http://www.omdbapi.com/?apikey=9addc862&s=" + searchTerm + "&plot=full";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
         $("#aDiv2").empty();
+        n = Math.floor((Math.random() * response.Search.length))
+        var drillDown = response.Search[n].imdbID;
+        var drillDownURL = "http://www.omdbapi.com/?apikey=9addc862&i=" + drillDown + "&plot=full";
+        $.ajax({
+            url: drillDownURL,
+            method: "GET"
+        }).then(function(response) {
+        console.log(response);
         var movieDiv = $("<div>");
         movieDiv.append("<h4>" + response.Title + "</h4>");
+        if(response.Poster !== "N/A") {
         movieDiv.append("<img style='width:100%' src='" + response.Poster + "'>");
+        };
         movieDiv.append("<h5>Directed by " + response.Director + "</h5>");
         movieDiv.append("<h5> Starring " + response.Actors + "</h5>");
         movieDiv.append("<h5> RT Rating: " + response.Ratings[0].Value + "</h5>");
         movieDiv.append("<p>" + response.Plot + "</p>");
         movieDiv.append("<a href='https://www.imdb.com/title/" + response.imdbID + "/'>View on IMDB</a>");
         $("#aDiv2").append(movieDiv);
+        });
     });
 };
 
